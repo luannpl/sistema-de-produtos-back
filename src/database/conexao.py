bef0cf2748590cd3d5ffae1872d5e26088cc8a6f
Paulo Luan <pauloluan1409@gmail.com>
2024-11-27 18:59:54 -0300

    incluindo banco de dados

--- src/database/conexao.py
+++ src/database/conexao.py
@@ -10,6 +10,8 @@ load_dotenv()
 host = os.getenv("DB_HOST")
 user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD")
+porta = os.getenv("DB_PORT")
+
 
 
 
@@ -18,12 +20,14 @@ def criar_conexao():
     conexao = mysql.connector.connect(
         host=host,
         user=user,
-        password=password
+        password=password,
+        port=porta,
+        database="railway"
     )
     cursor = conexao.cursor()
     try:
-        cursor.execute("CREATE DATABASE IF NOT EXISTS loja")
-        cursor.execute("USE loja")
+        cursor.execute("CREATE DATABASE IF NOT EXISTS railway")
+        cursor.execute("USE railway")
         criar_tabelas(cursor)
     except Exception as erro:
         print(f"{erro}: Erro ao criar o banco de dados")
