78bb6fe8c9a621a051a76c86d5ac8bfb2c2c4d08
luannpl <pauloluan1409@gmail.com>
2024-11-28 09:52:20 -0300

    start

--- src/database/conexao.py
+++ src/database/conexao.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 from src.database.tabelas import criar_tabelas
 
 # Carregar as variáveis de ambiente
-load_dotenv()
+# load_dotenv()
 
-# Dados de conexão com o banco de dados
-host = os.getenv("DB_HOST")
-user = os.getenv("DB_USER")
-password = os.getenv("DB_PASSWORD")
-porta = int(os.getenv("DB_PORT", 32461))
+# # Dados de conexão com o banco de dados
+# host = os.getenv("DB_HOST")
+# user = os.getenv("DB_USER")
+# password = os.getenv("DB_PASSWORD")
+# porta = int(os.getenv("DB_PORT", 32461))
 
 
 
@@ -18,12 +18,12 @@ porta = int(os.getenv("DB_PORT", 32461))
 def criar_conexao():
     # Criar uma conexão com o banco de dados
     conexao = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        port=porta,
-        database="railway"
-    )
+    host="junction.proxy.rlwy.net",
+    user="root",
+    password="UOhlaseowyczpaUnZpPhmMbfCsvfNSiC",
+    port=32461,
+    database="railway"
+)
     cursor = conexao.cursor()
     try:
         cursor.execute("CREATE DATABASE IF NOT EXISTS railway")
